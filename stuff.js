849238f2ef49160813db641b796e0a87f674ab09
Kjrey2003 <kajanirey@gmail.com>
2022-07-22 12:51:50 -0400

    added visible lines between the divs, and fixed up the divs so they stay in the same space with more boxes

--- stuff.js
+++ stuff.js
@@ -1,26 +1,36 @@
 const Black = document.getElementById("black") 
 let square = document.createElement('div');
+
 function putStuff(size){
 let board = document.querySelector(".pad");
+let squares = board.querySelectorAll("div");
+squares.forEach((div) => div.remove());
 board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for(let i = 0; i < 256; i++) {
+let amount = size * size;
+for(let i = 0; i < amount; i++) {
     let square = document.createElement('div');
     square.style.border = "1px solid black";
     square.style.backgroundColor = 'white';
     board.insertAdjacentElement('beforeend', square);
     }
-    return putStuff();
+     
+
 
 }
 
+
+
 putStuff(16);
 
+
+
+
+
+
  /*
- 
- 
- Black.addEventListener("click", function(){
+Black.addEventListener("click", function(){
     for(let i = 0; i < 256; i++) {
         let square = document.createElement('div');
         square.style.backgroundColor = 'black';
