976e7548d37699ba79610f8d76adbd6173e5f2e0
Kjrey2003 <kajanirey@gmail.com>
2022-07-22 10:35:58 -0400

    added functionality to some of the buttons, borders on each of the individuals divs. eveyrthing coming along kind of smooth right now

--- stuff.js
+++ stuff.js
@@ -1,3 +1,5 @@
+const Black = document.getElementById("black") 
+let square = document.createElement('div');
 function putStuff(size){
 let board = document.querySelector(".pad");
 board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -5,9 +7,28 @@ board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 for(let i = 0; i < 256; i++) {
     let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
+    square.style.border = "1px solid black";
+    square.style.backgroundColor = 'white';
     board.insertAdjacentElement('beforeend', square);
     }
+    return putStuff();
+
 }
 
 putStuff(16);
+
+ /*
+ 
+ 
+ Black.addEventListener("click", function(){
+    for(let i = 0; i < 256; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'black';
+        //board.insertAdjacentElement('beforeend', square);
+        }
+
+    
+ }
+ )
+
+ */
