7af4efcda112a8156846f795b651b28ccdb2a016
Kjrey2003 <kajanirey@gmail.com>
2022-07-25 13:25:57 -0400

    added a color picker and linked it to the function so pad actually works, just need to clean up the code and add a lil beauty

--- stuff.js
+++ stuff.js
@@ -1,44 +1,46 @@
-const Black = document.getElementById("black") 
+
+const instruct = document.getElementById("instructions");
+var old = instruct.innerHTML;
+
+
 let square = document.createElement('div');
-
-function putStuff(size){
-let board = document.querySelector(".pad");
-let squares = board.querySelectorAll("div");
-squares.forEach((div) => div.remove());
-board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-let amount = size * size;
-for(let i = 0; i < amount; i++) {
-    let square = document.createElement('div');
-    square.style.border = "1px solid black";
-    square.style.backgroundColor = 'white';
-    board.insertAdjacentElement('beforeend', square);
+ function putStuff(size){
+    if(size > 95){
+        document.getElementById('instructions').innerHTML = "Oops! The box cannot be bigger than 80, please reset and enter another number";
+        return;
     }
+
+let board = document.querySelector(".pad");
+ let squares = board.querySelectorAll("div");
+ squares.forEach((div) => div.remove());
+ board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+ board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+ 
+ let amount = size * size;
+ for(let i = 0; i < amount; i++) {
+     let square = document.createElement('div');
+    	
+     square.style.border = "1px solid black";
+     square.style.backgroundColor = 'white';
+     board.insertAdjacentElement('beforeend', square);
      
-
-
-}
-
-
-
-putStuff(16);
-
-
-
-
-
-
- /*
-Black.addEventListener("click", function(){
-    for(let i = 0; i < 256; i++) {
-        let square = document.createElement('div');
-        square.style.backgroundColor = 'black';
-        //board.insertAdjacentElement('beforeend', square);
-        }
-
+     
+     square.addEventListener("mouseover", function(){
+        square.style.backgroundColor = document.getElementById("color").value;
+       })
+    }
     
- }
- )
+    }
+    putStuff(16);
+ 
+   
+ 
+ 
+ 
 
- */
+ 
+
+
+ 
+
+
